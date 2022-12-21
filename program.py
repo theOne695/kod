7b8299c65c114f026190aaf2537243ce88a71bc8
theOne695 <115659409+theOne695@users.noreply.github.com>
2022-12-21 20:52:11 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -1,32 +1,32 @@
 import tabulate
 
-def user(): # Страница пользователя
+def user():
         print ("Это страница пользователя")
         spisok()
-def spisok(): # Страница товаров
+def spisok():
         print(" Список всех товаров: ")
         from tabulate import tabulate
         data = []
-        with open('list.txt',encoding="utf-8") as f: #открытие файла и добавление формата
+        with open('kod.txt',encoding="utf-8") as f:
             for line in f:
-             data.append(list(map(str.strip, line.split(',')))) #сама табуретка
-        print(tabulate(data, tablefmt='heavy_grid', headers=('Товар', 'Цена'))) #формат и что будет написано
+             data.append(list(map(str.strip, line.split(','))))
+        print(tabulate(data, tablefmt='grid', headers=('Товар', 'Цена')))
 
-#Страница добавления товаров
+
 def additem(Item=None, Prise=None):
         Item = input("Введи название Товара: ")
         Prise = input("Введи цену товара: ")
-        list = open("list.txt", "a", encoding="utf-8")                       # Открывает список товаров и ставит кадировку
-        list.write(Item+", "+str(Prise)+"\n")     #записывает товар и цену в файл
+        list = open("kod.txt", "a", encoding="utf-8")
+        list.write(Item+", "+str(Prise)+"\n")
         print("Товар успешно добавлен!")
-        list.close()                              #сохраняет и закрывает
-        admin()                                # кидает обратно в меню админа
+        list.close()
+        admin()
 
-#страница удаления товара
+
 def removeitem(Item1=None):
     spisok()
     Item1 = input ("Напиши что ты бы хотел удалить: ")
-    fn = 'list.txt'   #открывает лист
+    fn = 'kod.txt'   #открывает лист
     f = open(fn, encoding="utf-8")
     output = []
     for line in f:
@@ -42,32 +42,30 @@ def removeitem(Item1=None):
     ''')
     admin()
 
-#Страница редактирования товара
-#По факту он удаляет предмет и заново добавляет
 def replase(Newitem=None, NewName=None, Newprise=None):
     spisok()
     Newitem = input ("Напиши что ты бы хотел изменить: ")
-    fn = 'list.txt' #открывает лист
+    fn = 'kod.txt'
     f = open(fn, encoding="utf-8")
     output = []
     for line in f:
-        if not Newitem in line:      #удаляет newitem
+        if not Newitem in line:
             output.append(line)
-    f.close()          #закрывает лист
+    f.close()
     f = open(fn, 'w' , encoding="utf-8")
     f.writelines(output)
     f.close()
 
     NewName = input("Введи новое название товара: ")
     Newprise = input("Введи новую цену товара: ")
-    list = open("list.txt", "a", encoding="utf-8")                       # Открывает список товаров и ставит кадировку
-    list.write(NewName+", "+str(Newprise)+"\n")     #записывает товар и цену в файл
+    list = open("kod.txt", "a", encoding="utf-8")
+    list.write(NewName+", "+str(Newprise)+"\n")
     print("Товар успешно изменён!")
-    list.close()                              #сохраняет и закрывает
-    admin()                           # кидает обратно в меню админа
+    list.close()
+    admin()
 
 
-def admin(): # Главная страница админа
+def admin():
     print('''Добро пожаловать в пункт управления администатора! Выберите пункт меню:
     1. Добавить товар
     2. Удалить товар
@@ -82,16 +80,16 @@ def admin(): # Главная страница админа
         replase()
     elif user_input == '4':
         spisok()
-        admin()
+
     else:
         print("Ты что-то не так написал!")
 
-def login(Username=None, Password=None): # Страница логирования
+def login(Username=None, Password=None):
     Username = input("Введи свой логин: ")
     Password = input("Введи свой пароль: ")
 
-    if not len(Username or Password) < 1: # Проверка логина и пароля
-        db = open("database.txt", "r")  # Сравниние из БД
+    if not len(Username or Password) < 1:
+        db = open("database.txt", "r")
         d = []
         f = []
         for i in db:
@@ -102,7 +100,7 @@ def login(Username=None, Password=None): # Страница логировани
             f.append(b)
             data = dict(zip(d, f))
             if Username in data:
-                if data[Username][0] == 'a': #  Админ
+                if data[Username][0] == 'a':
                     hashed = data[Username].strip('a')
                 else:
                     hashed = data[Username].strip('b') # Юсер
@@ -112,21 +110,21 @@ def login(Username=None, Password=None): # Страница логировани
                 if (Password.encode(), hashed):
                     print("Login success!")
                     print("Привет", Username,"!")
-                    if data[Username][0] == 'a': # Если Админ, то кидает в строку админа
+                    if data[Username][0] == 'a':
                         admin()
-                    else:                        # Если не админ, то кидает в юсера
+                    else:
                         user()
 
                     break
-                else:  # Если не всё ок, то пишет следующее:
+                else:
                     print("Не правильный пароль!")
         if Username not in data:
             print("Такого логина не существует!")
     else:
         print("Пожалуйста, попробуйте войти ещё раз!")
         login()
-    db.close() #закрываем бд
-    # Страница регистрации
+    db.close()
+
 def register(Username=None, Password1=None, Password2=None):
     Username = input("Введите логин: ")
     Password1 = input("Придумайте пароль: ")
@@ -138,37 +136,35 @@ def register(Username=None, Password1=None, Password2=None):
         b = b.strip()
         c = a,b
         d.append(a)
-    if not len(Password1)<=3:                                # Нужно минимум 3 символа для регистрации
+    if not len(Password1)<=3:
         db = open("database.txt", "r")
         if not Username ==None:
-            if len(Username) <1:                              #Нужно минимум 1 символ для логина
+            if len(Username) <1:
                 print("Пожалуйста укажите логин: ")
                 register()
             elif Username in d:
-                print("Этот логин занят!")                 # Если такой логин уже есть в БД
+                print("Этот логин занят!")
                 register()
             else:
-                if Password1 == Password2:                              # Шифрует БД
+                if Password1 == Password2:
                     Password1 = Password1
 
 
-                    db = open("database.txt", "a")                       # Записывает в БД регистрацию
+                    db = open("database.txt", "a")
                     db.write(Username+", "+str(Password1)+"\n")
                     print("Ваш аккаунт успешно создан!!!")
                     print("Пожалуйста, войдите чтобы продолжить.")
 
-
-                # Если неправильно
                 else:
                     print("Пароли не совпадают!")
                     register()
     else:
         print("Ваш пароль слишком короткий!!!")
-    db.close() #нужно обазятельно закрывать бд, ибо если офф прогу то бд умрёт
+    db.close()
 
 
 
-def home(option=None):                                       # Главная страница
+def home(option=None):
     print('''Добро пожаловать! Выберите пункт меню:
     1. Регистрация
     2. Вход''')
@@ -180,6 +176,4 @@ def home(option=None):                                       # Главная с
     else:
         print("Ты ввел не то! Введи 1 или 2.")
 
-# Регистрация(Логин, Пароль1, Пароль2)
-# Авторизация(Логин, Пароль1)
 home()
